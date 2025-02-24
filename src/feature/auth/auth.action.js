a37668c5e3e41923d456a4bfe1acd3e0e86fb3ae
sajjad karim <sajukarim76@gmail.com>
2025-02-24 17:39:10 +0500

    Added: signup and signin redux

---
+++ src/feature/auth/auth.action.js
@@ -0,0 +1,54 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { supabase } from "../../lib/supabaseClient";
+
+export const signupUser = createAsyncThunk(
+  "auth/signupUser",
+  async (formData, { rejectWithValue }) => {
+    try {
+      const { data, error } = await supabase.auth.signUp({
+        email: formData.email,
+        password: formData.password,
+        options: {
+          data: { full_name: `${formData.firstName} ${formData.lastName}` },
+        },
+      });
+      if (error) {
+        return rejectWithValue(error.message);
+      }
+      const user = data.user;
+      if (!user) {
+        return rejectWithValue("User signup failed. Please try again.");
+      }
+      const { error: profileError } = await supabase
+        .from("profiles")
+        .insert([{ uid: user.id, role: formData.role }]);
+
+      if (profileError) {
+        return rejectWithValue(profileError.message);
+      }
+
+      return { message: "Signup successful! Please check your email." };
+    } catch (error) {
+      return rejectWithValue(
+        error.message || "Something went wrong. Please try again."
+      );
+    }
+  }
+);
+export const signinUser = createAsyncThunk(
+  "auth/signinUser",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email,
+        password,
+      });
+
+      if (error) return rejectWithValue(error.message);
+
+      return { user: data.user, message: "Sign in successful!" };
+    } catch (error) {
+      return rejectWithValue(error.message || "Something went wrong.");
+    }
+  }
+);
---
+++ src/feature/auth/auth.slicer.js
@@ -0,0 +1,68 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { signupUser, signinUser } from "./auth.action";
+const authSlice = createSlice({
+  name: "auth",
+  initialState: {
+    IsRegisterLoading: false,
+    IsRegisterFullfilled: false,
+    IsRegisterRejected: false,
+
+    IsLoginLoading: false,
+    IsLoginFullfilled: false,
+    IsLoginRejected: false,
+    success: null,
+    error: null,
+    user: null,
+  },
+  reducers: {
+    clearAuthMessages: (state) => {
+      state.IsRegisterLoading = false;
+      state.IsRegisterFullfilled = false;
+      state.IsRegisterRejected = false;
+      state.success = null;
+      state.error = null;
+      state.IsLoginLoading = false;
+      state.IsLoginFullfilled = false;
+      state.IsLoginRejected = false;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(signupUser.pending, (state) => {
+      state.IsRegisterLoading = true;
+      state.error = null;
+      state.success = null;
+    });
+    builder.addCase(signupUser.fulfilled, (state, action) => {
+      state.IsRegisterFullfilled = true;
+      state.IsRegisterLoading = false;
+      state.success = action.payload.message;
+    });
+    builder.addCase(signupUser.rejected, (state, action) => {
+      state.IsRegisterLoading = false;
+      state.IsRegisterFullfilled = false;
+      state.IsRegisterRejected = true;
+      state.error = action.payload;
+    });
+    // signin user
+    builder.addCase(signinUser.pending, (state) => {
+      state.IsLoginLoading = true;
+      state.error = null;
+      state.success = null;
+    });
+    builder.addCase(signinUser.fulfilled, (state, action) => {
+      state.IsLoginFullfilled = true;
+      state.IsLoginLoading = false;
+      state.user = action.payload.user;
+      state.success = action.payload.message;
+    });
+    builder.addCase(signinUser.rejected, (state, action) => {
+      state.IsLoginLoading = false;
+      state.IsLoginFullfilled = false;
+      state.IsLoginRejected = true;
+      state.error = action.payload;
+    });
+  },
+});
+
+export const { clearAuthMessages } = authSlice.actions;
+export default authSlice.reducer;
