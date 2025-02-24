a44682707637520f0cea4799473be75b8ec64277
Asad Ali Asim <asadaliasim909@gmail.com>
2025-02-25 02:22:12 +0500

    fix:profile

--- src/components/_Common/Grid.jsx
+++ src/components/_Common/Grid.jsx
@@ -100,7 +100,9 @@ function Grid() {
             </div>
           </>
         ) : (
-          <div className="w-10 h-10 rounded-full bg-gray-200" />
+          <Link to="/settings">
+            <div className="w-10 h-10 rounded-full bg-gray-200" />
+          </Link>
         )}
       </div>
 
