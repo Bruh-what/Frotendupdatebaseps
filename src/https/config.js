06e93f20c37712c655f2a5e94a31d48b8f5e5784
Asad Ali Asim <asadaliasim909@gmail.com>
2025-02-25 02:55:03 +0500

    url fixed

--- src/https/config.js
+++ src/https/config.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 // const backendUrl = import.meta.env.NEXT_PUBLIC_BACKEND_URL;
 // console.log(backendUrl);
 
-// const backendUrl = "https://backenddeploymentps-production.up.railway.app";
-const backendUrl = 'http://localhost:5000';
+const backendUrl = 'https://backenddeploymentps-production.up.railway.app';
+// const backendUrl = 'https://localhost:5000';
 
 export const BaseUrl = backendUrl;
 export const PROSPONSER = axios.create({ baseURL: BaseUrl });
