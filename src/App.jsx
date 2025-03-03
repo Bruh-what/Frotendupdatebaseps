132821d88a073711e5bc3bffdd4cd7c318fd6e6c
Asad Ali Asim <asadaliasim909@gmail.com>
2025-03-03 17:17:37 +0500

    fix: redux to main

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,44 @@
 'use client';
 import { Toaster } from 'react-hot-toast';
 import AppRoutes from './routes';
-import ReduxProvider from './store/Provider';
+
+import useProfileCheck from './hooks/useProfileCheck';
+import { useSelector } from 'react-redux';
 
 function App() {
+  useProfileCheck(); // Run profile check on app load
+
+  const isProfileIncomplete = useSelector(
+    (state) => state.auth.isProfileIncomplete
+  );
+
   return (
-    <ReduxProvider>
-      <Toaster position="bottom-right" reverseOrder={false} />
-      <AppRoutes />
-    </ReduxProvider>
+    <>
+      {isProfileIncomplete && (
+        <div
+          style={{
+            backgroundColor: '#FB6944',
+            color: 'white',
+            padding: '10px',
+            textAlign: 'center',
+            fontWeight: 'bold',
+            position: 'fixed',
+            top: 0,
+            left: 0,
+            width: '100%',
+            zIndex: 1000,
+          }}
+        >
+          Please complete your profile!
+        </div>
+      )}
+
+      {/* Add padding to avoid content overlap */}
+      <div style={{ paddingTop: isProfileIncomplete ? '50px' : '0px' }}>
+        <Toaster position="bottom-right" reverseOrder={false} />
+        <AppRoutes />
+      </div>
+    </>
   );
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from "react"
-import { createRoot } from "react-dom/client"
-import "./index.css"
-import App from "./App.jsx"
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
+import ReduxProvider from './store/Provider';
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
+createRoot(document.getElementById('root')).render(
+  <ReduxProvider>
     <App />
-  </StrictMode>
-)
+  </ReduxProvider>
+);
